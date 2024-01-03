0799c564aea266e60215f9ce300aa906bf2b44b4
ms <pms981125@naver.com>
2024-01-03 21:05:58 +0900

    서버 통신 문제 확인 필요

--- app/src/main/java/com/example/nbbang/ApiClient.kt
+++ app/src/main/java/com/example/nbbang/ApiClient.kt
@@ -11,29 +11,30 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 // ApiClient.kt
 object ApiClient {
 
-    private const val BASE_URL = "http://218.48.28.17:8080"
+    private const val BASE_URL = "http://211.212.148.6:8080"
 
     var gson = GsonBuilder()
         .setLenient()
         .create()
 
-    /*
     val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY//개발용
     }
 
-     */
-
-    /*
     val client = OkHttpClient.Builder()
         .addInterceptor(loggingInterceptor) // 로깅 인터셉터 추가
         .build()
 
-     */
+
+    /*val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .build()*/
 
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(ScalarsConverterFactory.create())
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
 
     val apiService = retrofit.create(ApiService::class.java)
--- app/src/main/java/com/example/nbbang/RoomViewModel.kt
+++ app/src/main/java/com/example/nbbang/RoomViewModel.kt
@@ -40,8 +40,24 @@ class RoomViewModel: ViewModel() {
     private fun updateList() {
         val userId = 1
 
-        for (i in 1..3) {
+        ApiClient.apiService.getRoom().enqueue(object: Callback<ApiService.RoomResponse>{
+            override fun onResponse(call: Call<ApiService.RoomResponse>, response: Response<ApiService.RoomResponse>) {
+                if (response.isSuccessful) {
+                    response.body()?.let { response.body()?.title?.let { it1 -> Item1(it1, it.info) } }
+                        ?.let { addItem(it) }
+                }
+            }
+            //서버와 통신 확인
+            override fun onFailure(call: Call<ApiService.RoomResponse>, t: Throwable) {
+                //Toast.makeText(this@LoginActivity, "통신 실패", Toast.LENGTH_SHORT).show()
+                println("%%%%%%%%%%%%%%%")
+                t.printStackTrace()
+                println("%%%%%%%%%%%%%%%")
+            }
+        })
+
+        /*for (i in 1..3) {
             addItem(Item1(i.toString(), i.toString()))
-        }
+        }*/
     }
 }
