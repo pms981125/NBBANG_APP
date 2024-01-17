38d77e86156834416c09b6707272c45779bcd318
ms <pms981125@naver.com>
2024-01-17 21:22:38 +0900

    Room 이동 구현

--- app/src/main/java/com/example/nbbang/MainActivity.kt
+++ app/src/main/java/com/example/nbbang/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.nbbang
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nbbang.databinding.ActivityMainBinding
 
@@ -25,6 +26,17 @@ class MainActivity : AppCompatActivity() {
             adapter.notifyDataSetChanged()
         }
         registerForContextMenu(binding.roomRecyclerView)
+
+        // ViewModel에서 LiveData를 구독
+        viewModel.itemClickEvent.observe(this, Observer { clickedPosition ->
+            // 클릭된 아이템의 위치 정보(clickedPosition)를 사용하여 원하는 동작 수행
+            // 예: 클릭된 아이템의 내용을 로그에 출력
+            //Log.d("RoomAdapter", "Item clicked at position $clickedPosition")
+            val intent = Intent(this, RoomActivity::class.java)
+            intent.putExtra("title", viewModel.items[clickedPosition].title)
+            intent.putExtra("contents", viewModel.items[clickedPosition].contents)
+            startActivity(intent)
+        })
 
         binding.fabCreateRoom.setOnClickListener {
             val intent = Intent(this, CreateRoomActivity::class.java)
